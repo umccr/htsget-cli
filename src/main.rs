9a9db475f993176e0c4c934f4c8b6b929826d5ce
Roman Valls Guimera <brainstorm@users.noreply.github.com>
2023-06-15 12:58:15 +1000

    Testing the return of encrypted regions/intervals

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
 use std::error::Error;
-use noodles_htsget as htsget;
+use std::str::FromStr;
+use noodles::htsget as htsget;
+use noodles::core::{Position, Region};
 use tokio::io::AsyncWriteExt;
 use tokio::io;
 use futures::TryStreamExt;
+use noodles::core::region::Interval;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let base_url = "http://localhost:8080/".parse()?;
     let client = htsget::Client::new(base_url);
 
-    let reads = client.reads("data/bam/htsnexus_test_NA12878");
+    let reads = client.reads("data/crypt4gh/htsnexus_test_NA12878").add_region(Region::new(
+        "11",
+        Interval::from_str("4999976-5003981").unwrap()
+    ));
     let reads = reads.send().await?;
 
     let mut chunks = reads.chunks();
